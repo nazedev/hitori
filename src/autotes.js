16185e508c34a5d638b9988befeadb60844aa5ad
danzprstt <gamelegend1092@gmail.com>
2025-02-20 16:07:58 +0700

    Create autotes.js

---
+++ src/autotes.js
@@ -0,0 +1,27 @@
+module.exports = {
+   start: async (m, { 
+      budy
+   }) => {
+      if (budy.match(/^\s*bang\s*$/)) {
+         await m.reply('Apa Monkey ?');
+      }
+   }   
+}
+module.exports = {
+   start: async (m, { 
+      budy
+   }) => {
+      if (budy.match(/^\s*danz\s*$/)) {
+         await m.reply('Apa Monkey ?');
+      }
+   }   
+}
+module.exports = {
+   start: async (m, { 
+      budy
+   }) => {
+      if (budy.match(/^\s*oyy\s*$/)) {
+         await m.reply('Apa Monkey ?');
+      }
+   }   
+}
