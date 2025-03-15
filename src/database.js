16ea6c472182e2f5c273684abe544dd7496ec27c
Kazuya-Labs <zuyabizon01@gmail.com>
2025-03-15 22:34:21 +0700

    fix conection mongodb

--- src/database.js
+++ src/database.js
@@ -1,85 +1,106 @@
-require('../settings');
-const fs = require('fs');
-const path = require('path');
-const chalk = require('chalk');
-const mongoose = require('mongoose');
+require("../settings");
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+
 let DataBase;
 
 if (/mongo/.test(global.tempatDB)) {
-	DataBase = class mongoDB {
-		constructor(url = global.tempatDB, options = { useNewUrlParser: true, useUnifiedTopology: true }) {
-			this.url = url
-			this.data = {}
-			this._model = {}
-			this.options = options
-		}
-		
-		read = async () => {
-			mongoose.connect(this.url, { ...this.options })
-			this.connection = mongoose.connection
-			try {
-				const schema = new mongoose.Schema({
-					data: {
-						type: Object,
-						required: true,
-						default: {},
-					}
-				})
-				this._model = mongoose.model('data', schema)
-			} catch {
-				this._model = mongoose.model('data')
-			}
-			this.data = await this._model.findOne({})
-			if (!this.data) {
-				new this._model({ data: {} }).save()
-				this.data = await this._model.findOne({})
-			} else return this?.data?.data || this?.data
-		}
-		
-		write = async (data) => {
-			if (this.data && !this.data.data) return (new this._model({ data })).save()
-			this._model.findById(this.data._id, (err, docs) => {
-				if (!err) {
-					if (!docs.data) docs.data = {}
-					docs.data = data
-					return docs.save()
-				}
-			})
-		}
-	}
+  class MongoDB {
+    constructor(
+      url = global.tempatDB,
+      options = { useNewUrlParser: true, useUnifiedTopology: true },
+    ) {
+      this.url = url;
+      this.data = {};
+      this._model = {};
+      this.options = options;
+      this.connection = null;
+    }
+
+    async connect() {
+      try {
+        this.connection = await mongoose.connect(this.url, this.options);
+        const schema = new mongoose.Schema({
+          data: {
+            type: Object,
+            required: true,
+            default: {},
+          },
+        });
+        this._model = mongoose.model("data", schema);
+      } catch (error) {
+        console.error(chalk.red("Connection error:", error));
+      }
+    }
+
+    async read() {
+      await this.connect();
+
+      this.data = await this._model.findOne({});
+      if (!this.data) {
+        await new this._model({ data: {} }).save();
+        this.data = await this._model.findOne({});
+      }
+      return this.data?.data || this.data;
+    }
+
+    async write(data) {
+      if (this.data && !this.data.data) {
+        return await new this._model({ data }).save();
+      }
+
+      try {
+        const docs = await this._model.findById(this.data._id);
+        if (!docs.data) docs.data = {};
+        docs.data = data;
+        return await docs.save();
+      } catch (error) {
+        console.error(chalk.red("Write error:", error));
+      }
+    }
+  }
+
+  DataBase = MongoDB;
 } else if (/json/.test(global.tempatDB)) {
-	DataBase = class dataBase {
-		data = {}
-		file = path.join(process.cwd(), 'database', global.tempatDB);
-		
-		read = async () => {
-			let data;
-			if (fs.existsSync(this.file)) {
-				data = JSON.parse(fs.readFileSync(this.file))
-			} else {
-				fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2))
-				data = this.data
-			}
-			return data
-		}
-		
-		write = async (data) => {
-			this.data = !!data ? data : global.db
-			let dirname = path.dirname(this.file)
-			if (!fs.existsSync(dirname)) fs.mkdirSync(dirname, { recursive: true })
-			fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2))
-			return this.file
-		}
-	}
+  class JSONDatabase {
+    constructor() {
+      this.data = {};
+      this.file = path.join(process.cwd(), "database", global.tempatDB);
+    }
+
+    async read() {
+      let data;
+      if (fs.existsSync(this.file)) {
+        data = JSON.parse(fs.readFileSync(this.file));
+      } else {
+        fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2));
+        data = this.data;
+      }
+      return data;
+    }
+
+    async write(data) {
+      this.data = data || global.db;
+      const dirname = path.dirname(this.file);
+      if (!fs.existsSync(dirname)) fs.mkdirSync(dirname, { recursive: true });
+      fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2));
+      return this.file;
+    }
+  }
+
+  DataBase = JSONDatabase;
 }
 
-module.exports = DataBase
+module.exports = DataBase;
 
+const file = require.resolve(__filename);
 
-let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update ${__filename}`))
-	delete require.cache[file]
-	require(file)
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update ${__filename}`));
+  delete require.cache[file];
+  require(file);
 });
+
