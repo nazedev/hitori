e4b7c0c196afaa0012fad1a15dafde593c698d86
Itznusa36 <itznusa@gmail.com>
2025-03-19 18:12:03 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner
+global.owner = ['62821138211p88'] //['628','628'] 2 owner
 global.packname = 'Bot WhatsApp'
 global.author = 'Nazedev'
 global.botname = 'Naze Bot'
