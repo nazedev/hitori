3324b5cf0402dfedfe8f0d5eb891de0d13337bd8
Terror-Machine <apaskipajaya@gmail.com>
2025-05-17 15:53:19 +0700

    update gift2webp schema

--- src/message.js
+++ src/message.js
@@ -9,7 +9,7 @@ const FileType = require('file-type');
 const PhoneNumber = require('awesome-phonenumber');
 
 const { checkStatus } = require('./database');
-const { imageToWebp, videoToWebp, writeExif } = require('../lib/exif');
+const { imageToWebp, videoToWebp, writeExif, gifToWebp } = require('../lib/exif');
 const { isUrl, getGroupAdmins, generateMessageTag, getBuffer, getSizeMedia, fetchJson, sleep, getTypeUrlMedia } = require('../lib/function');
 const { jidNormalizedUser, proto, getBinaryNodeChildren, getBinaryNodeChild, generateMessageIDV2, jidEncode, encodeSignedDeviceIdentity, generateWAMessageContent, generateForwardMessageContent, prepareWAMessageMedia, delay, areJidsSameUser, extractMessageContent, generateMessageID, downloadContentFromMessage, generateWAMessageFromContent, jidDecode, generateWAMessage, toBuffer, getContentType, WAMessageStubType, getDevice } = require('baileys');
 
